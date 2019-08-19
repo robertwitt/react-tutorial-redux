3516acc9404935b7c9d709251ce0353a928b132b
robertwitt <robert_witt@icloud.com>
2019-08-19 20:37:25 +0200

    Refactor to use action creators

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,67 +1,94 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
+import * as actionCreators from "../../store/actions/actions";
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+  state = {
+    counter: 0
+  };
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
+  counterChangedHandler = (action, value) => {
+    switch (action) {
+      case "inc":
+        this.setState(prevState => {
+          return { counter: prevState.counter + 1 };
+        });
+        break;
+      case "dec":
+        this.setState(prevState => {
+          return { counter: prevState.counter - 1 };
+        });
+        break;
+      case "add":
+        this.setState(prevState => {
+          return { counter: prevState.counter + value };
+        });
+        break;
+      case "sub":
+        this.setState(prevState => {
+          return { counter: prevState.counter - value };
+        });
+        break;
     }
+  };
 
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
-                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
-                <hr />
-                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Results</button>
-                <ul>
-                    {this.props.storedResults.map(storedResult => (
-                        <li key={storedResult.id} onClick={() => this.props.onDeleteResult(storedResult.id)}>{storedResult.value}</li>
-                    ))}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.props.onDecrementCounter}
+        />
+        <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+        <CounterControl
+          label="Subtract 5"
+          clicked={this.props.onSubtractCounter}
+        />
+        <hr />
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>
+          Store Results
+        </button>
+        <ul>
+          {this.props.storedResults.map(storedResult => (
+            <li
+              key={storedResult.id}
+              onClick={() => this.props.onDeleteResult(storedResult.id)}
+            >
+              {storedResult.value}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        ctr: state.ctr.counter,
-        storedResults: state.res.results
-    };
+  return {
+    ctr: state.ctr.counter,
+    storedResults: state.res.results
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onAddCounter: () => dispatch({type: actionTypes.ADD, val: 5}),
-        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 5}),
-        onStoreResult: result => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: id => dispatch({type: actionTypes.DELETE_RESULT, resultId: id})
-    };
-}
+  return {
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onAddCounter: () => dispatch(actionCreators.add(5)),
+    onSubtractCounter: () => dispatch(actionCreators.subtract(5)),
+    onStoreResult: result => dispatch(actionCreators.storeResult(result)),
+    onDeleteResult: id => dispatch(actionCreators.deleteResult(id))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
